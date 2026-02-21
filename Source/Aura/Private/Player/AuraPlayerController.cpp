e9aea3d79618f0b23314a2c43e7be3f505d28868
Jackson Silva <Jackson.ClementeSilva-ext@ab-Inbev.com>
2026-02-21 19:13:02 -0400

    Highlight enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,17 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -55,5 +62,60 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	{
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+	
+	/** 
+	 *  Line trace from cursor. There are several scenarios:
+	 *  A. LastActor is null && ThisActor is null
+	 *   - Do nothing
+	 *  B. LastActor is null && ThisActor is valid
+	 *   - Call ThisActor->OnBeginCursorOver() - Highlight ThisActor
+	 *  C. LastActor is valid && ThisActor is null
+	 *   - Call LastActor->OnEndCursorOver() - UnHighlight LastActor
+	 *  D. LastActor is valid && ThisActor is valid && LastActor != ThisActor
+	 *   - Call LastActor->OnEndCursorOver() - UnHighlight LastActor
+	 *   - Call ThisActor->OnBeginCursorOver() - Highlight ThisActor
+	 *  E. LastActor is valid && ThisActor is valid && LastActor == ThisActor
+	 *   - Do nothing
+	 */
+	
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			//Case B
+			ThisActor->HighlightActor();
+		}else
+		{
+			//Case A - Both are null, do nothing
+		}
+	}else
+	{
+		if (ThisActor == nullptr)
+		{
+			//Case C
+			LastActor->UnHighlightActor();
+		}else // both actors are valid
+		{
+			if (LastActor != ThisActor)
+			{
+				//Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}else
+			{
+				//Case E - Both actors are the same, do nothing
+			}
+		}
 	}
 }
