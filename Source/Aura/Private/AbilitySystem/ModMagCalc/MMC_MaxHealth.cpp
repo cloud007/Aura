3ec08b93ea7f9dc49eaa9558175d742962d3e7c9
Jackson Silva <Jackson.ClementeSilva-ext@ab-Inbev.com>
2026-03-21 20:26:48 -0400

    Modifier Magnitude Calculations

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -0,0 +1,43 @@
+// Copyright - Javackson Studio
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
+	
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	//Gather tags from source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Vigor = 0.f;
+	bool bCapturedAttributeMagnitude = GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParameters, Vigor);
+	
+	if (!bCapturedAttributeMagnitude)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UMMC_MaxHealth::CalculateBaseMagnitude_Implementation: Failed to capture Vigor attribute."));
+		return 0.f;
+	}
+	
+	Vigor = FMath::Max<float>(Vigor, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface ? CombatInterface->GetPlayerLevel() : 1;
+	
+	return 80.f + 2.5f * Vigor + 10.f * PlayerLevel;	
+}	
