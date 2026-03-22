3b9e8f210eecfc15346f8385a69d527b56f13e71
Jackson Silva <Jackson.ClementeSilva-ext@ab-Inbev.com>
2026-03-21 20:51:39 -0400

    Initialize Vital Attributes

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -10,8 +10,6 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(10.f);
-	InitMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
