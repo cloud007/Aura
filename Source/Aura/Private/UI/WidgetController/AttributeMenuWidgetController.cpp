8bd9b72ad3cb4d0a84e86091f08c526a375c9622
Jackson Silva <Jackson.ClementeSilva-ext@ab-Inbev.com>
2026-03-28 14:11:34 -0400

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright - Javackson Studio
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BrodcastInitialValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
