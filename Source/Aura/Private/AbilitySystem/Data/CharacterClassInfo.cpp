bf35c5ccf1fab6ba144afdbc34b47f2638d20025
Jackson Silva <Jackson.ClementeSilva-ext@ab-Inbev.com>
2026-04-05 19:35:14 -0400

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright - Javackson Studio
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
