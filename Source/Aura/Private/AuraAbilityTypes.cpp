2d5308c8d118ef26223ce9bd64ede5a70b64900e
Jackson Silva <Jackson.ClementeSilva-ext@ab-Inbev.com>
2026-04-11 19:31:25 -0400

    Implementing Net Serialize

--- Source/Aura/Private/AuraAbilityTypes.cpp
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -1,7 +1,25 @@
 
 #include "AuraAbilityTypes.h"
 
-bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
 {
+	FGameplayEffectContext::NetSerialize(Ar, Map, bOutSuccess);
+
+	uint8 RepBits = 0;
+	if (Ar.IsSaving())
+	{
+		if (bIsBlockedHit)  RepBits |= 1 << 0;
+		if (bIsCriticalHit) RepBits |= 1 << 1;
+	}
+
+	Ar.SerializeBits(&RepBits, 2);
+
+	if (Ar.IsLoading())
+	{
+		bIsBlockedHit  = (RepBits & (1 << 0)) != 0;
+		bIsCriticalHit = (RepBits & (1 << 1)) != 0;
+	}
+
+	bOutSuccess = true;
 	return true;
 }
--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -4,29 +4,49 @@
 #include "AuraAbilityTypes.generated.h"
 
 USTRUCT(BlueprintType)
-struct FAuraGameplayEffectContext: public FGameplayEffectContext 
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
 {
 	GENERATED_BODY()
-	
+
 public:
 	bool IsCriticalHit() const { return bIsCriticalHit; }
 	bool IsBlockedHit() const { return bIsBlockedHit; }
-	
-	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
-	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
-	
+
+	void SetIsCriticalHit(const bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(const bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
 	virtual UScriptStruct* GetScriptStruct() const override
 	{
 		return FAuraGameplayEffectContext::StaticStruct();
 	}
-	
+
+	virtual FGameplayEffectContext* Duplicate() const override
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
-	
+
 protected:
 	UPROPERTY()
 	bool bIsBlockedHit = false;
-	
+
 	UPROPERTY()
 	bool bIsCriticalHit = false;
-	
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true,
+	};
 };
